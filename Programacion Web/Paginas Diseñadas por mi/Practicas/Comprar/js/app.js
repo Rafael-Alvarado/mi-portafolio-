fe78528beea45febaca57c7efaf3938a443783a8
Rafael-Alvarado <rafaelalvarado286@gmail.com>
2020-02-20 15:26:13 -0430

    Añadido de todos los archivos

---
+++ Programacion Web/Paginas Diseñadas por mi/Practicas/Comprar/js/app.js
@@ -0,0 +1,6 @@
+var hola
+function accion(){
+    alert("Su compra ha sido realizada en unos momentos se le hara una factura");
+    alert("Mentira rolo e mamaguevo pa ti no hay nada te compraste fue una gigante bola de mierda jajajaja te estafe mmgvo!!");
+    
+}
